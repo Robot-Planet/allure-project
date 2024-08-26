ba5510938eb12fd128bc8403334cf39210b63d99
rBotPlanet <rbotplanet@gmail.com>
2024-08-26 14:41:41 +0700

    Add test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -31,6 +31,11 @@ test('has title', async ({ page }, TestInfo) => {
   console.log('Robot has title => ' + 'allure.');
 });
 
+//--- 2024-08-26, PR
+test('Allure PR', async ({ page }, TestInfo) => {
+  console.log('Robot has PR => ' + 'allure.');
+});
+
 test('get started link', async ({ page }, TestInfo) => {
   await page.goto('https://playwright.dev/');
   await page.waitForLoadState('load', {timeout: 30000});
